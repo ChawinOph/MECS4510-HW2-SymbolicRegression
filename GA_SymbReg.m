fb777e3ba49ca4c49d666b22863d91fde574b2a1
Chawin Ophaswongse <chawinoph@gmail.com>
2018-10-05 14:01:16 -0400

    first commit

---
+++ GA_SymbReg.m
@@ -0,0 +1,9 @@
+classdef GA_SymbReg
+    % SimpleGA Summary of this class goes here
+    %   This class is designed for a TSP
+    properties (Constant)
+        n_bit = 1000; % chormosome length
+    end
+     
+    
+end
