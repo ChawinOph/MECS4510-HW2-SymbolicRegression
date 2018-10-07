505d504b424e10276c51184e0f8bcd081ffef91d
Chawin Ophaswongse <chawinoph@gmail.com>
2018-10-07 18:33:12 -0400

    test again

--- GA_SymbReg.m
+++ GA_SymbReg.m
@@ -1,29 +1,26 @@
 classdef GA_SymbReg < handle
     % SimpleGA Summary of this class goes here
     %   This class is designed for a TSP
-    properties (Constant)
-        n_bit = 1000; % chormosome length
-    end
-    
     properties
         filename            % file name of the given set of points
         points              % 1000 x 2 set of f(x) = x points from the text file
-        down_sample_no = 1  % no. of skipped x points (use every other down_sample_no points)
-        max_heap_level      % maximum number of heap levels
+        down_sample_no      % no. of skipped x points (use every other down_sample_no points)
+        n_heap              % maximum number of heap levels
         n_pop               % number of population
-        pool                % n_bit x n_pop array of current chormosomes
+        pool                % n_heap  x n_pop array of current chormosomes (heaps)
+        
         p_c                 % cross over prob (single crossover)
         p_m                 % mutation prob
         n_crossover         % number of crossover indice of parent chromosomes (no. of cutting lines)
         n_mutation          % number of mutation indice in each parent chromosome
         n_iter              % number of evaluations/n_pop
-        trunc_rate          % truncation selection ratio to improve diversity (1/n fraction form only!)
-        b_find_longest      % boolean: choose goal
+        
+        trunc_rate          % truncation selection ratio to improve diversity (1/n fraction form only!
         n_tournament        % number of parents for competing before geeting only top two       
     end
     
     properties              % Dependent variables
-        fitness             % 1 x n_pop:  array of fitness correspoinding to each chromosome
+        fitness             % 1 x n_pop:  array of fitness correspoinding to each chromosome (mean absolute error)
         x_mat               % n_bit x n_pop array: repeated n_pop columns of x position of points
         y_mat               % n_bit x n_pop array: repeated n_pop columns of y position of points
         fittest             % 1 x n_eval:  array of best fitness value of each iteration
@@ -31,13 +28,14 @@ classdef GA_SymbReg < handle
     end
     
     methods
-        %% Constuctor
-        function this = GA_SymbReg(filename, down_sample_no, max_heap_size, n_pop)
+        %% Constructor
+        function this = GA_SymbReg(filename, down_sample_no, n_pop, n_heap)
             this.filename = filename;
             this.down_sample_no = down_sample_no;
             this.points = this.importPath(this.filename);
-            this.max_heap_size = max_heap_size;
             this.n_pop = n_pop;
+            this.n_heap = n_heap;
+%             this.pool = this.contructHeaps();
         end
         
         %% Member Functions   
@@ -50,25 +48,15 @@ classdef GA_SymbReg < handle
             elseif isfloat(csv_filename)
                 points = csv_filename;
             end
-            points = points(1: this.down_sample_no: end, :);
-            
+            points = points(1: this.down_sample_no: end, :);    
         end       
         
-        function contructHeaps(this)
-            % Randomly create the operator part
-            m = zeros(this.max_heap_level, this.n_pop);
-            m(1, :) = randi([1, 6], 1, this.n_pop);
-            m(2 : end, :) = randi([0, 6], this.max_heap_level - 1, this.n_pop);
-            
-            % Change all children under the zero parents to be zeros
-            
-            % Reduce children under sine/cosine to be only one
-            
-            % Add children back to the end of each branch (if sine/cosine add only one)
-            
-            % Use boolean function 
-            
-            % Add the end of each operator as                 
+        function heaps = contructHeaps(this)
+                  
+        end
+        
+        function mean_abs_err = calcFitness(this)
+                  
         end
         
         % Snipping function
--- main_SymbReg.m
+++ main_SymbReg.m
@@ -1,25 +1,126 @@
 %% Assignment 2: Symbolic Regression
-clc; 
+clc;
 close all;
 clear;
 %% GA
+
 down_sample_no = 10;
-max_heap_level = 2;
-n_pop = 5;
+n_heap = 3;
+n_pop = 10;
 n_crossover = 2;
 n_mutation = 1;
 n_eval = 1e3;
 
- GA = GA_SymbReg('function1.csv', down_sample_no);
- GA.plotXYScatter();
+GA = GA_SymbReg('function1.csv', down_sample_no, n_pop, n_heap);
+GA.plotXYScatter();
 
-m = zeros(2^max_heap_level, n_pop);
-m(1, :) = randi([1, 6], 1, n_pop);
-m(2 : 2^(max_heap_level) - 2,:) = randi([0, 6], 2^(max_heap_level) - 2, n_pop);
-m(end, :) = randi([0, 2], 1, n_pop);
+%%
 
-m_logic = m == 0;
+m = nan*ones(2^n_heap - 1, n_pop);
+m(1, :) = randi([11, 16], 1, n_pop);
+for i = 1 : 2^(n_heap) - 1
+    % Running through each index of the heap structure
+    if ~isempty(find(m(i, :)  == 11 | m(i, :) == 14, 1))
+        % operators +, -, /, *
+        if i <= 2^(n_heap - 2) - 1
+            m([2*i, 2*i + 1], m(i, :)  == 11 | m(i, :) == 14) = randi([11, 18], ...
+                2, size(m(m(i, :)  == 11 | m(i, :) == 14), 2));
+        else
+            % the row before the last row can have x or constant children
+            m([2*i, 2*i + 1], m(i, :)  == 11 | m(i, :) == 14) = randi([17, 18], ...
+                2, size(m(m(i, :)  == 11 | m(i, :) == 14), 2));
+        end
+    end
+    
+    if ~isempty(find(m(i, :)  == 12 | m(i, :) == 13, 1))
+        % operators +, -, /, *
+        if i <= 2^(n_heap - 2) - 1
+            m([2*i, 2*i + 1], m(i, :)  == 12 | m(i, :) == 13) = randi([11, 18], ...
+                2, size(m(m(i, :)  == 12 | m(i, :) == 13), 2));
+        else
+            % the row before the last row can have x or constant children
+            m([2*i, 2*i + 1], m(i, :)  == 12 | m(i, :) == 13) = randi([17, 18], ...
+                2, size(m(m(i, :)  == 12 | m(i, :) == 13), 2));
+        end
+    end
+    
+    if ~isempty(find(m(i, :) >= 15 & m(i, :) <= 16, 1))
+        % sin or cosine cases
+        if i <= 2^(n_heap - 2) - 1 % not the row before the last row
+            m(2*i, m(i, :) >= 15 & m(i, :) <= 16) = randi([11, 18], ...
+                1, size(m(m(i, :) >= 15 & m(i, :) <= 16), 2));
+        elseif i <= 2^(n_heap - 1) - 1 % the row before the last row can only have x or constant children
+            m(2*i, m(i, :) >= 15 & m(i, :) <= 16) = randi([17, 18], ...
+                1, size(m(m(i, :) >= 15 & m(i, :) <= 16), 2));
+        end
+        m(2*i + 1, m(i, :) >= 15 & m(i, :) <= 16) = nan;
+    end
+    
+    if ~isempty(find(m(i, :) == 17, 1))
+        % If the index is a terminal of a constant
+        % assign all zeros underneath an assign a contant at the 7
+        m(i, m(i, :) == 17) = 20*rand(size(find(m(i, :) == 17))) - 10;
+        if i <= 2^(n_heap - 2) - 1
+            % if still not the row before the last row, assign zeros
+            % underneath
+            m([2*i, 2*i + 1], m(i, :) == 17) = nan;
+        end
+    end
+    
+    if ~isempty(find(m(i, :) == 18, 1))
+        % If the index is a terminal (constant or x)
+        % assign all zeros underneath an assign a contant at the 7
+        m(i, m(i, :) == 18) = 20; % change to a number beyond the constant range
+        if i <= 2^(n_heap - 2) - 1
+            % if still not the row before the last row, assign zeros
+            % underneath
+            %             m([2*i, 2*i + 1], m(i, :) == 18) = zeros(2, size(m(m(i, :) == 18), 2));
+            m([2*i, 2*i + 1], m(i, :) == 18) = nan;
+        end
+    end
+    
+end
 
+GA.pool = m;
 
+%% evaluate the matrix
+% replace 20's by a value of x
+y_mat = zeros(length(GA.points), GA.n_pop);
 
+for n = 1: length(GA.points)
+    m = GA.pool;
+    m(m == 20) = GA.points(n, 1);
 
+for i = 2^(n_heap) - 2 : - 2 :1
+    if ~isempty(find(m(i/2, :) == 11, 1)) % check if there are any "+"
+        m(i/2 , m(i/2, :) == 11) = m(i, m(i/2, :) == 11) + m(i + 1, m(i/2, :) == 11);
+    end
+    
+    if ~isempty(find(m(i/2, :) == 12, 1)) % check if there are any "-"
+        m(i/2 , m(i/2, :) == 12) = m(i, m(i/2, :) == 12) - m(i + 1, m(i/2, :) == 12);
+    end
+    
+    if ~isempty(find(m(i/2, :) == 13, 1)) % check if there are any "/"
+        m(i/2 , m(i/2, :) == 13) = m(i, m(i/2, :) == 13)./m(i + 1, m(i/2, :) == 13);
+    end
+    
+    if ~isempty(find(m(i/2, :) == 14, 1)) % check if there are any "*"
+        m(i/2 , m(i/2, :) == 14) = m(i, m(i/2, :) == 14).*m(i + 1, m(i/2, :) == 14);
+    end
+    
+    if ~isempty(find(m(i/2, :) == 15, 1)) % check if there are any "sine"
+        m(i/2 , m(i/2, :) == 15) = sin(m(i, m(i/2, :) == 15));
+    end
+    
+    if ~isempty(find(m(i/2, :) == 16, 1)) % check if there are any "cosine"
+        m(i/2 , m(i/2, :) == 16, :) = cos(m(i, m(i/2, :) == 16));
+        m([i, i + 1], m(i/2, :) == 16) = nan;
+    end
+end
+   y_mat(n, :) = m(1, :);
+end
+
+% find MAE (fitness) for each chromosome
+y_func_mat = repmat(GA.points(:,2), 1, GA.n_pop);
+mean_abs_err = mean(abs(y_func_mat - y_mat));
+sort(mean_abs_err)
